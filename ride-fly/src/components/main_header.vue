9d8abb54a07871aae28a5192bb645b3e94a4fd36
adinda vanderzande <adindavanderzande@gmail.com>
2023-03-28 16:38:06 +0200

    add_mainheader_comp

---
+++ ride-fly/src/components/main_header.vue
@@ -0,0 +1,27 @@
+<script setup>
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+    <header>
+      <h1>{{ title }}</h1>
+      <DocumentationIcon />
+    </header>
+  </template>
+  
+  <script>
+  export default {
+  name: 'title',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+    ,
+    iconUrl: {
+      type: String,
+      required: true
+    }
+  }
+}
+  </script>
