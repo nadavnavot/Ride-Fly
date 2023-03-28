ac1ebc9729ec19065eb0b421a39400975d843d09
adinda vanderzande <adindavanderzande@gmail.com>
2023-03-28 16:37:10 +0200

    add_currentdate_comp

---
+++ ride-fly/src/components/current_date.vue
@@ -0,0 +1,22 @@
+<script>
+import DocumentationIcon from './icons/IconDocumentation.vue'
+</script>
+
+<template>
+    <header>
+      <DocumentationIcon />
+      <h1>25/1 Wednesday {{ Date }}</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+  name: 'Date',
+  props: {
+    arr: {
+    type: Date,
+    default: function () { return new Date() }
+  }
+  }
+}
+  </script>
