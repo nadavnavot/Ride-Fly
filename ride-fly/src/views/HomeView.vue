77bc0fb42847e20b3caa5416555c4285fd5a45d4
adinda vanderzande <adindavanderzande@gmail.com>
2023-03-28 17:39:24 +0200

    updatehome

--- ride-fly/src/views/HomeView.vue
+++ ride-fly/src/views/HomeView.vue
@@ -2,12 +2,15 @@
 export default {
   components: {
     main_header,
-    current_date
+    current_date,
+    availableride_card
 }  
 }
 
 import main_header from '../components/main_header.vue';
 import current_date from '../components/current_date.vue';
+import availableride_card from '../components/availableride_card.vue';
+
 </script>
 
 <template>
@@ -16,5 +19,6 @@ import current_date from '../components/current_date.vue';
     </main_header>
     <current_date>
     </current_date>
+    <availableride_card></availableride_card>
   </div>
 </template>
