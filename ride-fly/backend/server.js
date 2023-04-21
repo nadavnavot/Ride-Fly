58485cac69594958ee985b642e4353b413cea881
adinda vanderzande <adindavanderzande@gmail.com>
2023-04-21 14:42:07 +0200

    CORS_install

--- ride-fly/backend/server.js
+++ ride-fly/backend/server.js
@@ -1,7 +1,11 @@
 // Add modules
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http,
+  cors: {
+  origin: 'http://example.com',
+  methods: ['GET', 'POST']
+});
 const mongoose = require("mongoose");
 let users = [];
 let messages = [];
