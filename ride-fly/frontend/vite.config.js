19b57d1881bb6308c9c72f65f1992e27631c5214
adinda vanderzande <adindavanderzande@gmail.com>
2023-04-23 23:14:03 +0200

    vue3_socketio_installed

--- ride-fly/frontend/vite.config.js
+++ ride-fly/frontend/vite.config.js
@@ -6,9 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
